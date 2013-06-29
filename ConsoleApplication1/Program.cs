3efca5c711c58fe89f7bfa54fb82027839172dff
Boris Dachev <bdachev@gmail.com>
2013-06-29 22:55:08 +0300

    Remove stall code

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,16 +10,6 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var sol = Babylon.LocalizationSolution.LocalizationSolutionSerializer.DeserializeSolution(@"S:\SOURCE\Trio\MP v3\Chinese Translation\MPv3_Minhua_CHS.btp");
-            var items = from prj in sol.Projects.OfType<Babylon.VSProject.VSLocalizationProject>()
-                        from ri in prj.Items
-                        from li in ri.LocaleItems
-                        where li.Text == null
-                        select new {prj, ri, li};
-            foreach (var item in items)
-            {
-                Console.WriteLine("Prj:{0}, Res:{1}, Loc:{2}", item.prj.Name, item.ri.Name, item.li.Locale);
-            }
             Console.ReadLine();
         }
     }
