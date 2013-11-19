c69a66cf4132c4bf977d1e77097c42a306f4b491
Boris Dachev <bdachev@gmail.com>
2013-11-19 16:46:23 +0200

    Cleaned up Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleApplication1.Properties;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,19 +10,6 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var settingsMain = Settings.Default;
-            settingsMain.Test = "main domain";
-            settingsMain.Save();
-            var domain = AppDomain.CreateDomain(AppDomain.CurrentDomain.FriendlyName);
-            domain.DoCallBack(() =>
-            {
-                var settingsDomain = Settings.Default;
-                Console.WriteLine("Test={0}", settingsDomain.Test);
-                settingsDomain.Test = "other domain";
-                settingsDomain.Save();
-            });
             Console.ReadLine();
         }
     }
