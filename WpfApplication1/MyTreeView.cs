f2f9957b7a5f94a796af88a2b32c7053b2d9d778
Boris Dachev <bdachev@gmail.com>
2014-04-02 17:33:14 +0300

    tree test

---
+++ WpfApplication1/MyTreeView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApplication1
+{
+    public class MyTreeView : TreeView
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new MyTreeViewItem();
+        }
+    }
+}
