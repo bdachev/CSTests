11d0fb08e2a9f3666f3c84391ebe20ca10123196
Boris Dachev <bdachev@gmail.com>
2015-06-10 17:03:20 +0300

    Added wix bootstrapper and setup projects

---
+++ BootstrapperCore/Application.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Threading;
+
+namespace MyBootstrapperCore
+{
+    public class Application : BootstrapperApplication
+    {
+        protected override void Run()
+        {
+            var mainWindow = new MainWindow();
+            Engine.Detect();
+            mainWindow.Closed += (o, e) => mainWindow.Dispatcher.InvokeShutdown();
+            mainWindow.Show();
+            Dispatcher.Run();
+        }
+    }
+}
